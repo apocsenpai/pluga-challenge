49fc2c4e13559faa6ab9f34a6d322d725b49bd67
apocsenpai <sennasjonatas@gmail.com>
2023-12-19 13:15:20 -0300

    refactor: change extension tsx to ts in styled archives

---
+++ src/components/CardList/index.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const CardList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-auto-rows: 11.125rem;
+  gap: 3rem 1.5rem;
+  padding: 2rem;
+`;
