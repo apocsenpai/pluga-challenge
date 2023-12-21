934d55d524c72149c19d34dc55e73465ef51c7a6
apocsenpai <sennasjonatas@gmail.com>
2023-12-21 10:58:51 -0300

    tests: implements input unit tests

--- src/components/atoms/Input/__tests__/index.spec.tsx
+++ src/components/atoms/Input/__tests__/index.spec.tsx
@@ -1,19 +1,19 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { Input } from '..';
 
 const PLACEHOLDER_MOCK = 'PLACEHOLDER_MOCK';
+const PLACEHOLDER_COLOR_ACTIVE = '#0ea7ff';
 
 describe('<Input>', () => {
   it('should render a Input that is not focused', () => {
     const { getByPlaceholderText } = render(
-      <Input placeholder={PLACEHOLDER_MOCK} focused={false} />,
+      <Input placeholder={PLACEHOLDER_MOCK} $focused={false} />,
     );
 
     const inputElement = getByPlaceholderText(PLACEHOLDER_MOCK);
 
     expect(inputElement).toBeInTheDocument();
     expect(inputElement).toHaveAttribute('placeholder', PLACEHOLDER_MOCK);
-
   });
 });
